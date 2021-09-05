892847ca8b08dab706eee9ef4acecfbbd19c77e5
KeichiTS <kkeichi@gmail.co>
2021-09-05 02:32:44 -0300

    bug_fix

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -5,13 +5,15 @@ export var speed = 100
 var dir = Vector2(0,-1) setget set_dir
 export var hp = 2
 export var pt = 10
-
+enum {death,alive}
+var status = alive
 func _ready():
 	pass
 	
 func _process(delta):
 	var pos = (target - position).normalized()
-	translate(pos*speed*delta)
+	if status == alive:
+		translate(pos*speed*delta)
 	look_at(target)
 
 func set_dir(val):
@@ -23,6 +25,7 @@ func _on_enemy_area_entered(area):
 		area.queue_free()
 		hp -= 1
 		if hp == 0:
+			status = death
 			$CollisionShape2D.disabled = true
 			POINTS.points += pt
 			Music.play_explosion()

3c6fec6577111b8929eba1656ec4ecf94160d75c
KeichiTS <kkeichi@gmail.co>
2021-09-05 02:16:51 -0300

    a_stupid_bug_solved

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -5,17 +5,14 @@ export var speed = 100
 var dir = Vector2(0,-1) setget set_dir
 export var hp = 2
 export var pt = 10
-enum {alive,dead}
-var status = alive
 
 func _ready():
 	pass
 	
 func _process(delta):
-	if status == alive:
-		var pos = (target - position).normalized()
-		translate(pos*speed*delta)
-		look_at(target)
+	var pos = (target - position).normalized()
+	translate(pos*speed*delta)
+	look_at(target)
 
 func set_dir(val):
 	dir = val 
@@ -25,12 +22,10 @@ func _on_enemy_area_entered(area):
 	if area.is_in_group("bullet"):
 		area.queue_free()
 		hp -= 1
-		if hp <= 0:
-			if status == alive:
-				POINTS.points += pt
-			status = dead
-			Music.play_explosion()
+		if hp == 0:
 			$CollisionShape2D.disabled = true
+			POINTS.points += pt
+			Music.play_explosion()
 			$anim.play("anim")
 			yield($anim,"animation_finished")
 			queue_free()
